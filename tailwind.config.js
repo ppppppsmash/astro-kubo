8edf30ef322e5096de83ec82a6d645567314fe9d
ppppppsmash <pppppp.work@gmail.com>
2024-05-31 23:01:36 +0900

    feat: language button

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,6 +62,20 @@ module.exports = {
         primary: ["var(--font-primary)", fontPrimaryType],
         secondary: ["var(--font-secondary)", fontSecondaryType],
       },
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+
     },
   },
   plugins: [
