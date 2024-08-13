80c5b2edbfbc6dadf390c8d9f90e45951c330c48
ppppppsmash <pppppp.work@gmail.com>
2024-08-13 20:57:35 +0900

    feat: change pic for top

--- src/layouts/components/ParallaxPanel.tsx
+++ src/layouts/components/ParallaxPanel.tsx
@@ -11,57 +11,26 @@ export const products = [
     title: "テスト",
     link: "#",
     thumbnail:
-      "https://placehold.jp/600x500.png",
+      "/images/landing_pic1.jpg",
   },
   {
     title: "テスト",
     link: "#",
     thumbnail:
-      "https://placehold.jp/600x500.png",
+      "/images/landing_pic2.jpg",
   },
   {
     title: "テスト",
     link: "#",
     thumbnail:
-      "https://placehold.jp/600x500.png",
-  },
-
-  {
-    title: "テスト",
-    link: "#",
-    thumbnail:
-      "https://placehold.jp/600x500.png",
-  },
-  {
-    title: "テスト",
-    link: "#",
-    thumbnail:
-      "https://placehold.jp/600x500.png",
-  },
-  {
-    title: "テスト",
-    link: "#",
-    thumbnail:
-      "https://placehold.jp/600x500.png",
+      "/images/landing_pic3.jpg",
   },
 
   {
     title: "テスト",
     link: "#",
     thumbnail:
-      "https://placehold.jp/600x500.png",
-  },
-  {
-    title: "テスト",
-    link: "#",
-    thumbnail:
-      "https://placehold.jp/600x500.png",
-  },
-  {
-    title: "テスト",
-    link: "#",
-    thumbnail:
-      "https://placehold.jp/600x500.png",
+      "/images/landing_pic4.jpg",
   },
   {
     title: "テスト",
@@ -99,5 +68,5 @@ export const products = [
     link: "#",
     thumbnail:
       "https://placehold.jp/600x500.png",
-  },
+  }
 ];
