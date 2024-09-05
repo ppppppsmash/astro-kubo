aa81aedafaca0922f3117ffbf3c4715e281b15d8
ppppppsmash <pppppp.work@gmail.com>
2024-09-05 18:52:43 +0900

    fix: Business width

--- src/layouts/components/InfiniteMovingCardsComponent.tsx
+++ src/layouts/components/InfiniteMovingCardsComponent.tsx
@@ -1,8 +1,8 @@
 "use client";
- 
+
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./react-ui/infinite-moving-cards";
- 
+
 export function InfiniteMovingCardsDemo({ service_list }: { service_list: any }) {
   return (
     <div className="relative overflow-hidden mb-14">
