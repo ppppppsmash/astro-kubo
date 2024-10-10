781e69c44035320be3f19ab594f30794bf92ad1c
ppppppsmash <pppppp.work@gmail.com>
2024-10-10 21:28:17 +0900

    feat: apply backgroundlines to event component

--- src/layouts/components/BackgroundLines.tsx
+++ src/layouts/components/BackgroundLines.tsx
@@ -19,7 +19,7 @@ export const BackgroundLines = ({
   return (
     <div
       className={cn(
-        "h-[20rem] md:h-screen w-full bg-white dark:bg-black",
+        "w-full",
         className
       )}
     >
